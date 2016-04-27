33bad0e520704f1190816dd370e39a17de3bbf12
Daiwei <daiweilu123@gmail.com>
2016-04-27 10:57:36 -0400

    Remove typings from git

--- typings/lodash.d.ts
+++
@@ -1,7 +0,0 @@
-declare module 'lodash/get' {
-  export default function get<T>(object: Object, path: (string|number)[] | string): T;
-}
-
-declare module 'lodash/isEqual' {
-  export default function isEqual(value: any, other: any): boolean;
-}
--- typings/main.d.ts
+++
@@ -1,2 +0,0 @@
-/// <reference path="lodash.d.ts" />
-/// <reference path="main/ambient/rx/index.d.ts" />
